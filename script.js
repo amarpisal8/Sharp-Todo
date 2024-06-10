8fa0b5a4a00f842810fa4941e3051ba461a4044f
Amar Pisal <109473825+amarpisal8@users.noreply.github.com>
2024-06-10 12:04:04 +0000

    apiUrl Update

--- script.js
+++ script.js
@@ -1,87 +1,84 @@
-const apiUrl = "https://crudcrud.com/api/332ff279073643059d148e910af32498";
+const apiUrl = "https://crudcrud.com/api/a6970ea3638c447a86049b0e242ee75a";
 
 window.addEventListener("DOMContentLoaded", () => {
-  axios.get(`${apiUrl}/Data`)
-    .then((res) => {
-      res.data.forEach(item => ShowUserOnScreen(item));
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
-  axios.get(`${apiUrl}/Done`)
-    .then((res) => {
-      res.data.forEach(item => AddToDone(item));
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    axios.get(`${apiUrl}/Data`)
+        .then((res) => {
+            res.data.forEach(user => ShowUserOnScreen(user));
+        })
+        .catch((err) => console.log(err));
+    axios.get(`${apiUrl}/Done`)
+        .then((res) => {
+            res.data.forEach(user => AddToDone(user));
+        })
+        .catch((err) => console.log(err));
 });
 
 function handleFormSubmit(event) {
-  event.preventDefault();
-  const todo = event.target.username.value;
-  const description = event.target.Description.value;
-  const myObj = { Todo: todo, description: description };
+    event.preventDefault();
+    const Todo = event.target.username.value;
+    const description = event.target.Description.value;
+    const myObj = { Todo, description };
 
-  axios.post(`${apiUrl}/Data`, myObj)
-    .then((res) => {
-      ShowUserOnScreen(res.data);
-      event.target.reset();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    axios.post(`${apiUrl}/Data`, myObj)
+        .then((res) => {
+            ShowUserOnScreen(res.data);
+            event.target.reset();
+        })
+        .catch((err) => console.log(err));
 }
 
 function ShowUserOnScreen(user) {
-  const tableBody = document.getElementById("listOfItems");
-  const row = document.createElement("tr");
+    const ParentEle = document.getElementById("listOfItems");
+    const row = document.createElement("tr");
 
-  row.innerHTML = `
-    <td>${user.Todo}</td>
-    <td>${user.description}</td>
-    <td class="action-btns">
-      <button class="btn btn-success" onclick="markAsDone('${user._id}', '${user.Todo}', '${user.description}')">✔️</button>
-      <button class="btn btn-danger" onclick="deleteUser('${user._id}', this)">🗑️</button>
-    </td>
-  `;
+    row.innerHTML = `
+        <td>${user.Todo}</td>
+        <td>${user.description}</td>
+        <td>
+            <button class="btn btn-success" onclick="markAsDone('${user._id}', '${user.Todo}', '${user.description}')">
+                <i class="fas fa-check"></i>
+            </button>
+            <button class="btn btn-danger" onclick="deleteUser('${user._id}')">
+                <i class="fas fa-trash-alt"></i> 
+            </button>
+        </td>
+    `;
 
-  tableBody.appendChild(row);
+    row.dataset.id = user._id;
+    ParentEle.appendChild(row);
 }
 
-function deleteUser(userId, btn) {
-  axios.delete(`${apiUrl}/Data/${userId}`)
-    .then(() => {
-      const row = btn.closest("tr");
-      row.remove();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+function deleteUser(userId) {
+    axios.delete(`${apiUrl}/Data/${userId}`)
+        .then(() => removeFromScreen(userId))
+        .catch((err) => console.log(err));
+}
+
+function removeFromScreen(userId) {
+    const row = document.querySelector(`[data-id='${userId}']`);
+    if (row) {
+        row.remove();
+    }
 }
 
 function markAsDone(userId, todo, description) {
-  const myObj = { Todo: todo, description: description };
-
-  axios.post(`${apiUrl}/Done`, myObj)
-    .then(() => {
-      deleteUser(userId, { closest: () => document.querySelector(`button[onclick*='${userId}']`).closest("tr") });
-      AddToDone(myObj);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    const myObj = { todo, description };
+    axios.post(`${apiUrl}/Done`, myObj)
+        .then(() => {
+            deleteUser(userId);
+            AddToDone(myObj);
+        })
+        .catch((err) => console.log(err));
 }
 
 function AddToDone(user) {
-  const tableBody = document.getElementById("DoneItems");
-  const row = document.createElement("tr");
+    const doneEle = document.getElementById("DoneItems");
+    const row = document.createElement("tr");
 
-  row.innerHTML = `
-    <td>${user.Todo}</td>
-    <td>${user.description}</td>
-  `;
+    row.innerHTML = `
+        <td>${user.todo}</td>
+        <td>${user.description}</td>
+    `;
 
-  tableBody.appendChild(row);
+    doneEle.appendChild(row);
 }
